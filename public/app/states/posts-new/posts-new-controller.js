38892470a1f713d160e025b6210fa8814f6baf56
Jeremy <jperez@kalkomey.com>
2014-09-17 14:34:21 -0500

    I now get a response from submitting a form.

--- public/app/states/posts-new/posts-new-controller.js
+++ public/app/states/posts-new/posts-new-controller.js
@@ -1,6 +1,17 @@
 angular
 .module('KnowledgeBase')
-.controller('PostsNewController', function($scope) {
+.controller('PostsNewController', function($scope, $http) {
 
-  $scope.orderProp = 'updated_at';
+  $http.get('http://localhost:3000/api/v1/categories.json').success(function(dataa) {
+    $scope.categories = dataa;
+  });
+
+  // we store all the article data from the form in this object
+  $scope.articleData = {};
+
+  // function to process the form
+  $scope.processForm = function() {
+      alert('awesome!');
+  };
+
 });
