deeafcbe469df9da0980cd0b541a979a2db6707e
Jeremy <jperez@kalkomey.com>
2014-11-05 11:55:19 -0600

    now you can create tags from the api

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,26 @@
 class TagsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    # @posts = Post.all
+    @tags = Tag.all.order("id DESC")
+    # @posts.each do |post|
+    #   raise post.category
+    # end
+    respond_to do |format|
+      format.json {render :json => @tags}
+    end
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+
+    respond_to do |format|
+      format.json {render :json => @tag}
+    end
+  end
+
+
   def new
     # default: render 'new' template
     @tag = Tag.new
@@ -6,14 +28,18 @@ class TagsController < ApplicationController
 
   def create
     @tag = Tag.new(tag_params)
-    if @tag.save
-      flash[:notice] = "#{@tag.name} was successfully created!"
-      redirect_to new_post_path
-    else
-      flash[:error] = "Tag failed to create."
-      redirect_to new_post_path
+
+    respond_to do |format|
+      if @tag.save
+        format.json { render json: @tag, status: :created }
+        format.xml { render xml: @tag, status: :created }
+      else
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+        format.xml { render xml: @tag.errors, status: :unprocessable_entity }
+      end
     end
   end
+
 
   private
     def tag_params
